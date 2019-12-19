690239fc83ad6e10723e92b7f891e0cd585fbf29
BR3TT03 <rockbrett03@gmail.com>
2019-12-19 11:08:14 +0545

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import login from '../views/login.vue'
+import register from '../views/register.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    component: register
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
---
+++ src/views/register.vue
@@ -0,0 +1,59 @@
+<template>
+      <v-dialog 
+      v-model="dialog" 
+      persistent 
+      max-width="400"
+      no-click-animation
+      overlay-opacity=1
+      >
+        <v-card tile>
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Famas Registration</v-toolbar-title>
+                <v-spacer />
+                
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">REGISTER</v-btn>
+              </v-card-actions>
+            </v-card>
+          <!-- </v-col>
+        
+      </v-container> -->
+      </v-dialog>
+</template> 
+
+<script>
+  export default {
+    data(){
+      return{
+        dialog:'true',
+      }
+    },
+    props: {
+      source: String,
+    },
+  }
+</script> 
